c6a571ae690cef4066edfe4d3667a0cad84adc17
Lucia Jimena Martin Rodriguez <lucia-ma@c2r9s3.42madrid.com>
2022-12-09 18:50:57 +0100

    so

--- main.c
+++ main.c
@@ -1,35 +1,42 @@
 #include "pipex.h"
+#include <fcntl.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv, char **envp)
 {
     int ncomand;
     pid_t pid; 
-	int	fd[2][2];
+	int	fd[argc - 3][2];
 	char **paths;
     int file[2];
-	char *path;
 	int i;
 	int p;
-
-	i = 0;
+	if( argc < 5)
+	{
+		write(1, "faltan argumentos", 18);
+		return(0);
+	}
 	pid = 1;
+	i = 0;
     ncomand = 1;
 
 	/////CREAR EL PATH
 	paths = (ft_split(ft_find_paths(envp,  "PATH"), ':'));
+	i = 0;
 	*paths += +5;
 	p = 0;
-	i = 0;
 	//..............................
 
 	/////CREAR LOS HIJOS Y LOS PIPES
     while(ncomand <= argc - 3)
     {
-		//printf("%d\n", fd[i][0]);
-		pipe(fd[i]);
         pid = fork();
+		pipe(fd[i]);
         if(pid == 0)
+		{
+			printf("%d\n", ncomand);
             break;
+		}
         if(pid < 0)
             return(0);
         ncomand ++;
@@ -37,49 +44,46 @@ int main(int argc, char **argv, char **envp)
     }
 	//.........................
 	
-	///        ###stdin == 0    ·····     ###stdout == 1      
+	///        ###stdin == 0    ·····     ###stdout == 1    
+	
 	///FORMATEAR SALIDAS     
     if (ncomand == 1)
 	{
-		// int errnum = 0;
-		// printf("%d\n", fd[i][0]);
-		// if (dup2(STDOUT_FILENO, fd[i][1]) < 0)
-		// 	printf("error %s\n", strerror(errnum));
 		file[0] = open(argv[0], O_RDONLY);
-		if (dup2(STDIN_FILENO, file[0]) < 0)
+		if (dup2(file[0], STDIN_FILENO) < 0)
 			return(0);
-		if (dup2(STDOUT_FILENO, fd[i][1]) < 0)
+		if (dup2(fd[i][1], STDOUT_FILENO) < 0)
 			return(0);
+		dprintf(STDOUT_FILENO, "hfiuehwoffheo    dffijeiorj3oi   %d\n", fd[i][1]);
 		close(fd[i][0]);
-		printf("hola soy %d% d\n", pid, fd[i][1]);
 	}
 
 	else if (ncomand == argc - 3)
 	{
 		file[1] = open(argv[argc - 1], O_WRONLY);
-		if (dup2(STDOUT_FILENO, file[1]) < 0)
+		if (dup2(file[1], STDOUT_FILENO) < 0)
 			return(0);
-		if (dup2(STDIN_FILENO, fd[i][0]) < 0)
+		if (dup2(fd[i-1][0], STDIN_FILENO) < 0)
 			return(0);
-        close(fd[i][1]);
+		char *s;
+
+		s = malloc(5 * 1);
+		printf("hola guapa %zd\n", read(STDIN_FILENO, s, 5));
+
+		write(2, s, 5);
+	    close(fd[i][1]);
+		close(fd[i][0]);
 	}
 
 	if(pid != 0 && ncomand != argc - 3 && ncomand != 1)
 	{
-		if (dup2(STDIN_FILENO, fd[i][0]) < 0)
+		if (dup2(fd[i-1][0], STDIN_FILENO) < 0)
 			return(0);
-		if (dup2(STDOUT_FILENO, fd[i][1]) < 0)
+		if (dup2( fd[i][1], STDOUT_FILENO) < 0)
 			return(0);
 	}
-		// printf("jojijijijii == %d\n", file[1]);
 
 	i = 0;
-	//.......................................
-	// while(paths[x])
-	// {
-	// 	printf("%s\n", paths[x]);
-	// 	x ++;
-	// }
 	if(pid == 0)
 	{
 		////PASAR LOS COMANDOS A ARRAY BIDIMENSIONAAL
@@ -90,21 +94,12 @@ int main(int argc, char **argv, char **envp)
 		/////METER EN LOS PATHS EL LA PRIMERA PALABRA DEL COMANDO
 		while(paths[i])
 		{
-			//printf("antes == %s\n", paths[i]);
 			paths[i] = ft_strjoin(paths[i], comando[0]);
-			// printf("despues == %s\n", paths[i]);
-			// printf("%d\n", access(paths[i], X_OK));
 			if(access(paths[i], X_OK) != -1)
-			{
-				printf("its ok =)\n");
 				break;
-			}
 			i ++;
 		}
-		printf("comando == %s path[i] == %s\n", *comando, paths[i]);
-		// printf("hola soy == %d mi path es == %s\n", i, paths[i]);
 		execve(paths[i], comando, envp);
-		// printf("señorita poch\n");
 	}
 	wait(NULL);
 	return(0);
--- t.c
+++ t.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-int main()
+extern int counter;
+
+// int main(void) {
+//     int fd = open("archivo2.txt", O_WRONLY | O_APPEND);
+
+//     printf("archivo2.txt fd = %d\n", fd);
+//     dup2(fd, 121);
+   
+//  dprintf(121, "This string will be printed in archivo2.txt file\n");
+//     exit(EXIT_SUCCESS);
+// }
+
+int main(void)
 {
-	printf("%d", stdin);
-	return(0);
+	int pid;
+	int fd[2][2];
+	int x = 2;
+	while(x--)
+	{
+		pid = fork();
+		pipe(fd);
+	}
+	
 }
