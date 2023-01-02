c801733042fcfe5700745955b9b2666db03b0568
luciamar1 <hola.tuttttiiiii.24@gmail.com>
2023-01-02 15:07:23 +0100

    cambios creados a simple vista sincompilar ft_execute(comando heredoc) creacion de malloc fd y ft_close_all

--- pepo.c
+++ pepo.c
@@ -4,6 +4,16 @@
 #include <fcntl.h>
 #include <errno.h>
 
+void    ft_close_all(tt_list *pipex)
+{
+    int counter;
+    counter = pipex->ncomand;
+    while(counter-- <= pipex->ncomand)
+    {
+        close(pipex->fd[counter][0]);
+        close(pipex->fd[counter][1]);
+    }
+}
 void	ft_heredoc(int *file, char *delimitador)
 {
 	char *gnl;
@@ -86,6 +96,8 @@ int    final_child(tt_list *pipex, int *file, char **argv, int argc)
     	return(0);
     close(pipex->fd[pipex->comand][0]);
     close(pipex->fd[pipex->comand][1]);
+    close(pipex->fd[(pipex->comand) - 1][1]);
+    //ft_close_all(pipex);
     return(1);
 }
 
@@ -95,7 +107,11 @@ void    ft_execute(tt_list *pipex, char **argv, char **envp)
     int i;
 
     i = 0;
-	comando = ft_splitpip(argv[pipex->comand + 1], ' ');
+	if(pipex->heredoc == 0)
+        comando = ft_splitpip(argv[pipex->comand + 3], ' ');
+    else
+        comando = ft_splitpip(argv[pipex->comand + 2], ' ');
+    
     while(pipex->paths[i])
     {
     	pipex->paths[i]= ft_strjoinpip(pipex->paths[i], comando[0]);
@@ -128,20 +144,19 @@ void    creationchilds(tt_list *pipex, int argc, char **argv, char **envp)
    
    i = 0;
    x = pipex->ncomand;
-   while(x)
+    if((pipex->fd = malloc((argc - 3) * sizeof(int *))) == NULL)  
+        return(0);
+   while(x--)
    {
-        pipe(pipex->fd[i]);
-        printf("puta x = %d\n", x);
-        printf("puta i = %d\n", i);
-        x--;
-        i ++;
+        if((pipex->fd[i] = malloc(2 * (sizeof(int)))) == NULL)
+            return(0);
+        pipe(pipex->fd[i++]);
    }
-    x = 2;
     i = 0;
 
     while(pipex->comand < pipex->ncomand)
     {
-    pipex->pid = fork();
+        pipex->pid = fork();
         if(pipex->pid == 0)
             break;
         if(pipex->pid < 0)
@@ -163,17 +178,6 @@ int main(int argc, char **argv, char **envp)
     pipex.comand = 0;
     pipex.heredoc = ft_strncmp("here_doc", argv[1], 9);
     pipex.ncomand = argc - 3;
-    if((pipex.fd = malloc((argc - 3) * sizeof(int *))) == NULL)  
-        return(0);
-    //FALTA UN WHILE
-    if((*(pipex.fd) = malloc(2 * (sizeof(int)))) == NULL)
-        return(0);
-    printf("joptetitas\n");
-    pipex.fd[0][0] = 69;
-    pipex.fd[0][1] = 96;
-    pipex.fd[1][0] = 6;
-    pipex.fd[1][1] = 9;
-    printf("fd == %d\n%d\n%d\n%d\n",  pipex.fd[0][0],  pipex.fd[0][1],  pipex.fd[1][0],  pipex.fd[1][1]);
     pipex.paths = (ft_splitpip(ft_find_paths(envp,  "PATH"), ':'));
     *(pipex.paths) += 5;
     if (pipex.heredoc == 0)
