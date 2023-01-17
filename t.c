4f5f69f2af8f3d37bbf47d656727b87cf98057fc
luciamar1 <hola.tuttttiiiii.24@gmail.com>
2023-01-17 15:30:53 +0100

    term

--- t.c
+++ t.c
@@ -5,7 +5,30 @@
 
 ///formatear salidas
 
+int ft_execute(tt_list *pipex, char **argv, char **envp)
+{
+    ////PASAR LOS COMANDOS A ARRAY BIDIMENSIONAAL
+	char **comando;
+    int i;
+	
+    i = 0;
+    comando = ft_splitpip(argv[comand + 1], ' ');
+	//................................
+
+	/////METER EN LOS PATHS EL LA PRIMERA PALABRA DEL COMANDO
+	while(pipex->paths[i])
+	{
+		pipex->paths[i] = ft_strjoinpip(pipex->paths[i], comando[0]);
+		if(access(pipex->paths[i], X_OK) != -1)
+			break;
+		i ++;
+	}
+		
+	execve(pipex->paths[i], comando, envp);
+}
+
 ///PRIMERO
+
 int first_child(tt_list *pipex, int *file, char **argv)
 {
     if(pipex->heredoc == 0)
@@ -49,7 +72,22 @@ int middle_child(tt_list *pipex)
 
 
 ///ULTIMO
-
+int final_child(tt_list *pipex, int *file, argv, argc)
+if (pipex->pid == 0 && (pipex->comand == argc - 4 || (pipex->heredoc == 0 && pipex->comand == argc - 5)))
+{
+	file[1] = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if(file[1] < 0)
+	{
+		perror("error");
+		return(0);
+	}
+	if (dup2(file[1], STDOUT_FILENO) < 0)
+		return(0);
+	if (dup2(fd[pipex->comand-1][0], STDIN_FILENO) < 0)
+		return(0);
+	close(fd[pipex->comand][0]);
+	close(fd[pipex->comand][1]);
+}
 ///crear hijos
 void ft_place_comand(tt_list *pipex, int argc, char **argv, char **envp)
 {
@@ -104,6 +142,7 @@ int main(int argc, char **argv, char **envp)
     pipex.heredoc = argc - 3; 
     pipex.comand = 0;
     pipex.ncomand = argc - 3;
+    //puede q cuando haya heredoc comand empieze en 2
     if (pipex.heredoc == 0)
         pipex.ncomand = argc - 4;
     pipex.paths = (ft_splitpip(ft_find_paths(envp,  "PATH"), ':'));
@@ -112,5 +151,10 @@ int main(int argc, char **argv, char **envp)
     ft_create_childs_fd(&pipex);
     ft_place_comand(&pipex, argc, argv, envp);
     printf("hola soy el hijo == %d\n", pipex.comand);
+    while(i < 0)
+    {
+        wait(NULL);
+        ncomand --;
+    }   
     return (0);
 }
